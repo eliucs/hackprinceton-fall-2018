f0d57f550e21a3f28cd557372646f804ec685fd0
Eric Liu <ericliu314@gmail.com>
2018-11-10 11:18:16 -0500

    Fix random_words

--- flask-service/word_generation/random_words.py
+++ flask-service/word_generation/random_words.py
@@ -5,17 +5,6 @@ def generate_random_words(n = 1):
   if n < 1:
     raise RuntimeError('n must be a number >= 1')
 
-  totalWords = len(WORD_LIST)
-
-  # Generate n unique, random, indices between 0 and TOTAL_WORDS
-  indices = set()
-  while len(indices) < n:
-    randomIndex = random.randint(0, totalWords - 1)
-    if randomIndex not in indices:
-        indices.add(randomIndex)
-
-  # Add words by index to result array
-  result = list(map(lambda index: WORD_LIST[index], list(indices)))
+  result = random.sample(WORD_LIST, n)
   random.shuffle(result)
-
   return result
