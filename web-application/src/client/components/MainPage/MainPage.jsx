d8e6e3314121e6a011a787da7d364e08eab843f0
Eric Liu <ericliu314@gmail.com>
2018-11-10 22:27:00 -0500

    Add login/signup

--- web-application/src/client/components/MainPage/MainPage.jsx
+++ web-application/src/client/components/MainPage/MainPage.jsx
@@ -1,14 +1,106 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { appStyles } from './styles.scss';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
+import {
+  btnLogin,
+  card,
+  cardTitle,
+  childMainPage,
+  invalid,
+  parentMainPage,
+  linkSignup,
+} from './styles.scss';
 
-const MainPage = props => (
-  <div className={appStyles}>
-  </div>
-);
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+    };
+  }
 
-MainPage.propTypes = {
-  title: PropTypes.string,
-};
+  onFieldChange = (type, e) => {
+    const text = e.target.value;
+    if (type === 'email') {
+      const email = text.trim();
+      this.setState({ email });
+    } else {
+      this.setState({ password: text });
+    }
+  };
+
+  handleLogin = () => {
+    this.setState({ error: '' });
+    // Make request to server to handle login
+    console.log(this.state.email, this.state.password);
+  };
+
+  render() {
+    return (
+      <div className={parentMainPage}>
+        <div className={childMainPage}>
+          <Container fluid>
+            <Row>
+              <Col md="2" />
+              <Col md="8">
+                <div className={card}>
+                  <div className={cardTitle}>Login to audioCAPTCHA</div>
+                  { this.state.error
+                    && <div className={invalid}>Invalid email or password.</div> }
+                  <Form>
+                    <FormGroup>
+                      <Label for="email">Email</Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="johnsmith@example.com"
+                        onChange={e => this.onFieldChange('email', e)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="password">Password</Label>
+                      <Input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        onChange={e => this.onFieldChange('password', e)}
+                      />
+                    </FormGroup>
+                  </Form>
+                  <Button
+                    color="primary"
+                    onClick={this.handleLogin}
+                    className={btnLogin}
+                  >
+                    Login
+                  </Button>
+                  <div>
+                    <Link className={linkSignup} to="/signup">Sign up for an account.</Link>
+                  </div>
+                </div>
+              </Col>
+              <Col md="2" />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+MainPage.propTypes = {};
 
 export default MainPage;
--- web-application/src/client/components/SignupPage/SignupPage.jsx
+++ web-application/src/client/components/SignupPage/SignupPage.jsx
@@ -1,15 +1,106 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { appStyles } from './styles.scss';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
+import {
+  btnSignup,
+  card,
+  cardTitle,
+  childMainPage,
+  invalid,
+  parentMainPage,
+  linkLogin,
+} from './styles.scss';
 
-const SignupPage = props => (
-  <div className={appStyles}>
-    <h1>{props.title}</h1>
-  </div>
-);
+class SignupPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+    };
+  }
 
-SignupPage.propTypes = {
-  title: PropTypes.string,
-};
+  onFieldChange = (type, e) => {
+    const text = e.target.value;
+    if (type === 'email') {
+      const email = text.trim();
+      this.setState({ email });
+    } else {
+      this.setState({ password: text });
+    }
+  };
+
+  handleSignup = () => {
+    this.setState({ error: '' });
+    // Make request to server to handle signup
+    console.log(this.state.email, this.state.password);
+  };
+
+  render() {
+    return (
+      <div className={parentMainPage}>
+        <div className={childMainPage}>
+          <Container fluid>
+            <Row>
+              <Col md="2" />
+              <Col md="8">
+                <div className={card}>
+                  <div className={cardTitle}>Sign up for audioCAPTCHA</div>
+                  { this.state.error
+                    && <div className={invalid}>Invalid email or password.</div> }
+                  <Form>
+                    <FormGroup>
+                      <Label for="email">Email</Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="johnsmith@example.com"
+                        onChange={e => this.onFieldChange('email', e)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="password">Password</Label>
+                      <Input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        onChange={e => this.onFieldChange('password', e)}
+                      />
+                    </FormGroup>
+                  </Form>
+                  <Button
+                    color="primary"
+                    onClick={this.handleSignup}
+                    className={btnSignup}
+                  >
+                    Sign Up
+                  </Button>
+                  <div>
+                    <Link className={linkLogin} to="/login">Login to your account.</Link>
+                  </div>
+                </div>
+              </Col>
+              <Col md="2" />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+SignupPage.propTypes = {};
 
 export default SignupPage;
